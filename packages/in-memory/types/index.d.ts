7fb6c3072d942ca9c1df0e600163cb571091e59e
Paul Sherman <paul.sherman.88@gmail.com>
2017-11-24 23:57:39 -0600

    Track in-memory types

---
+++ packages/in-memory/types/index.d.ts
@@ -0,0 +1,11 @@
+import { History, HickoryLocation, PartialLocation, AnyLocation, Options as RootOptions } from '@hickory/root';
+export { History, HickoryLocation, PartialLocation, AnyLocation };
+export interface Options extends RootOptions {
+    locations?: Array<string | PartialLocation>;
+    index?: number;
+}
+export interface InMemoryHistory extends History {
+    locations: Array<HickoryLocation>;
+    index: number;
+}
+export default function InMemory(options?: Options): InMemoryHistory;
