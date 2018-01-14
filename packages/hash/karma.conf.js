e89738a4a01996f989c051372c434f35c37792a4
Paul Sherman <paul.sherman.88@gmail.com>
2018-01-14 14:28:15 -0600

    Karma hash use default timeout

--- packages/hash/karma.conf.js
+++ packages/hash/karma.conf.js
@@ -1,69 +1,69 @@
-const typescript = require('rollup-plugin-typescript2');
-const replace = require('rollup-plugin-replace');
-const resolve = require('rollup-plugin-node-resolve');
+const typescript = require("rollup-plugin-typescript2");
+const replace = require("rollup-plugin-replace");
+const resolve = require("rollup-plugin-node-resolve");
 
-const reporters = ['progress'];
-const frameworks = ['jasmine'];
+const reporters = ["progress"];
+const frameworks = ["jasmine"];
 const plugins = [
-  'karma-jasmine',
-  'karma-chrome-launcher',
-  'karma-rollup-preprocessor'
+  "karma-jasmine",
+  "karma-chrome-launcher",
+  "karma-rollup-preprocessor"
 ];
 
 var customLaunchers = {
   // windows
   bs_chrome: {
-    base: 'BrowserStack',
-    browser: 'chrome',
-    browser_version: '57',
-    os: 'Windows',
-    os_version: '10'
+    base: "BrowserStack",
+    browser: "chrome",
+    browser_version: "57",
+    os: "Windows",
+    os_version: "10"
   },
   bs_firefox: {
-    base: 'BrowserStack',
-    browser: 'firefox',
-    browser_version: '53',
-    os: 'Windows',
-    os_version: '10'
+    base: "BrowserStack",
+    browser: "firefox",
+    browser_version: "53",
+    os: "Windows",
+    os_version: "10"
   },
   bs_ie_11: {
-    base: 'BrowserStack',
-    browser: 'ie',
-    browser_version: '11.0',
-    os: 'Windows',
-    os_version: '10'
+    base: "BrowserStack",
+    browser: "ie",
+    browser_version: "11.0",
+    os: "Windows",
+    os_version: "10"
   },
   bs_edge: {
-    base: 'BrowserStack',
-    browser: 'edge',
-    browser_version: '13',
-    os: 'Windows',
-    os_version: '10'
+    base: "BrowserStack",
+    browser: "edge",
+    browser_version: "13",
+    os: "Windows",
+    os_version: "10"
   },
   // os x
   bs_osx_safari: {
-    base: 'BrowserStack',
-    browser: 'safari',
-    browser_version: '10',
-    os: 'OS X',
-    os_version: 'Sierra'
+    base: "BrowserStack",
+    browser: "safari",
+    browser_version: "10",
+    os: "OS X",
+    os_version: "Sierra"
   },
   // ios
   bs_ios_safari: {
-    base: 'BrowserStack',
-    browser: 'Safari',
-    os: 'iOS',
-    os_version: '10.3',
-    device: 'iPhone 7',
+    base: "BrowserStack",
+    browser: "Safari",
+    os: "iOS",
+    os_version: "10.3",
+    device: "iPhone 7",
     real_mobile: true
   },
   // android
   bs_android_chrome: {
-    base: 'BrowserStack',
-    browser: 'chrome',
-    os: 'android',
-    os_version: '7.0',
-    device: 'Samsung Galaxy S8',
+    base: "BrowserStack",
+    browser: "chrome",
+    os: "android",
+    os_version: "7.0",
+    device: "Samsung Galaxy S8",
     real_mobile: true
   }
 };
@@ -71,54 +71,50 @@ var customLaunchers = {
 module.exports = function(config) {
   config.set({
     customLaunchers: customLaunchers,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     frameworks: frameworks,
     reporters: reporters,
 
-    files: [
-      'tests/integration/*.ts'
-    ],
+    files: ["tests/integration/*.ts"],
 
     preprocessors: {
-     'tests/integration/*.ts': ['rollup']
+      "tests/integration/*.ts": ["rollup"]
     },
 
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      "text/x-typescript": ["ts", "tsx"]
     },
 
     rollupPreprocessor: {
       plugins: [
         typescript({
-          tsconfig: 'tests/integration/tsconfig.json'
+          tsconfig: "tests/integration/tsconfig.json"
         }),
         resolve()
       ],
-      format: 'iife',
-      sourcemap: 'inline'
+      format: "iife",
+      sourcemap: "inline"
     },
 
     plugins: plugins
-    
   });
 
   if (process.env.TRAVIS) {
     config.set({
       browsers: Object.keys(customLaunchers),
       singleRun: true,
-      reporters: reporters.concat('BrowserStack'),
-      plugins: plugins.concat('karma-browserstack-launcher'),
+      reporters: reporters.concat("BrowserStack"),
+      plugins: plugins.concat("karma-browserstack-launcher"),
       browserStack: {
-        project: 'hickory',
+        project: "hickory",
         username: process.env.BROWSERSTACK_USERNAME,
         accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
         build: process.env.TRAVIS_BUILD_NUMBER,
         name: process.env.TRAVIS_JOB_NUMBER,
-        video: false,
-        timeout: 120
+        video: false
       },
       browserDisconnectTimeout: 10000,
       browserDisconnectTolerance: 1
     });
   }
-}
+};
