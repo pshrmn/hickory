d17ca0f5763957f58f1e3cc3ccb0edf19c557054
Paul Sherman <paul.sherman.88@gmail.com>
2018-09-05 15:07:58 -0500

    Ship .mjs ESM builds (#10)

--- packages/hash/scripts/build.js
+++ packages/hash/scripts/build.js
@@ -1,30 +1,30 @@
-const rollupBuild = require('../../../scripts/build');
+const rollupBuild = require("../../../scripts/build");
 
 // don't bundle dependencies for es/cjs builds
-const pkg = require('../package.json')
+const pkg = require("../package.json");
 const deps = Object.keys(pkg.dependencies).map(key => key);
-const depsString = deps.join(',');
+const depsString = deps.join(",");
 
 rollupBuild(
-  'ES',
-  { f: 'es', o: 'dist/hickory-hash.es.js', e: depsString },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "ESM",
+  { f: "esm", o: "dist/hickory-hash.mjs", e: depsString },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  'CommonJS',
-  { f: 'cjs', o: 'dist/hickory-hash.common.js', e: depsString },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "CommonJS",
+  { f: "cjs", o: "dist/hickory-hash.js", e: depsString },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> file',
-  { f: 'iife', o: 'dist/hickory-hash.js' },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "<script> file",
+  { f: "iife", o: "dist/hickory-hash.umd.js" },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> min file',
-  { f: 'iife', o: 'dist/hickory-hash.min.js' },
-  { NODE_ENV: 'production', BABEL_ENV: 'build' }
+  "<script> min file",
+  { f: "iife", o: "dist/hickory-hash.min.js" },
+  { NODE_ENV: "production", BABEL_ENV: "build" }
 );
--- packages/location-utils/scripts/build.js
+++ packages/location-utils/scripts/build.js
@@ -1,25 +1,25 @@
-const rollupBuild = require('../../../scripts/build');
+const rollupBuild = require("../../../scripts/build");
 
 rollupBuild(
-  'ES',
-  { f: 'es', o: 'dist/hickory-location-utils.es.js' },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "ESM",
+  { f: "esm", o: "dist/hickory-location-utils.mjs" },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  'CommonJS',
-  { f: 'cjs', o: 'dist/hickory-location-utils.common.js' },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "CommonJS",
+  { f: "cjs", o: "dist/hickory-location-utils.js" },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> file',
-  { f: 'iife', o: 'dist/hickory-location-utils.js' },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "<script> file",
+  { f: "iife", o: "dist/hickory-location-utils.umd.js" },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> min file',
-  { f: 'iife', o: 'dist/hickory-location-utils.min.js' },
-  { NODE_ENV: 'production', BABEL_ENV: 'build' }
+  "<script> min file",
+  { f: "iife", o: "dist/hickory-location-utils.min.js" },
+  { NODE_ENV: "production", BABEL_ENV: "build" }
 );
