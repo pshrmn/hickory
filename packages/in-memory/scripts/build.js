8e346f1e0fc68214cca04f2356a2c27e5e344595
Paul Sherman <paul.sherman.88@gmail.com>
2018-09-05 13:51:47 -0500

    Ship .mjs ESM builds

--- packages/in-memory/scripts/build.js
+++ packages/in-memory/scripts/build.js
@@ -1,30 +1,30 @@
-const rollupBuild = require('../../../scripts/build');
+const rollupBuild = require("../../../scripts/build");
 
 // don't bundle dependencies for es/cjs builds
-const pkg = require('../package.json')
+const pkg = require("../package.json");
 const deps = Object.keys(pkg.dependencies).map(key => key);
-const depsString = deps.join(',');
+const depsString = deps.join(",");
 
 rollupBuild(
-  'ES',
-  { f: 'es', o: 'dist/hickory-in-memory.es.js', e: depsString },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "ESM",
+  { f: "esm", o: "dist/hickory-in-memory.mjs", e: depsString },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  'CommonJS',
-  { f: 'cjs', o: 'dist/hickory-in-memory.common.js', e: depsString },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "CommonJS",
+  { f: "cjs", o: "dist/hickory-in-memory.js", e: depsString },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> file',
-  { f: 'iife', o: 'dist/hickory-in-memory.js' },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "<script> file",
+  { f: "iife", o: "dist/hickory-in-memory.umd.js" },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> min file',
-  { f: 'iife', o: 'dist/hickory-in-memory.min.js' },
-  { NODE_ENV: 'production', BABEL_ENV: 'build' }
+  "<script> min file",
+  { f: "iife", o: "dist/hickory-in-memory.min.js" },
+  { NODE_ENV: "production", BABEL_ENV: "build" }
 );
--- packages/root/scripts/build.js
+++ packages/root/scripts/build.js
@@ -1,30 +1,30 @@
-const rollupBuild = require('../../../scripts/build');
+const rollupBuild = require("../../../scripts/build");
 
 // don't bundle dependencies for es/cjs builds
-const pkg = require('../package.json')
+const pkg = require("../package.json");
 const deps = Object.keys(pkg.dependencies).map(key => key);
-const depsString = deps.join(',');
+const depsString = deps.join(",");
 
 rollupBuild(
-  'ES',
-  { f: 'es', o: 'dist/hickory-root.es.js', e: depsString },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "ESM",
+  { f: "esm", o: "dist/hickory-root.mjs", e: depsString },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  'CommonJS',
-  { f: 'cjs', o: 'dist/hickory-root.common.js', e: depsString },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "CommonJS",
+  { f: "cjs", o: "dist/hickory-root.js", e: depsString },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> file',
-  { f: 'iife', o: 'dist/hickory-root.js' },
-  { NODE_ENV: 'development', BABEL_ENV: 'build' }
+  "<script> file",
+  { f: "iife", o: "dist/hickory-root.umd.js" },
+  { NODE_ENV: "development", BABEL_ENV: "build" }
 );
 
 rollupBuild(
-  '<script> min file',
-  { f: 'iife', o: 'dist/hickory-root.min.js' },
-  { NODE_ENV: 'production', BABEL_ENV: 'build' }
+  "<script> min file",
+  { f: "iife", o: "dist/hickory-root.min.js" },
+  { NODE_ENV: "production", BABEL_ENV: "build" }
 );
