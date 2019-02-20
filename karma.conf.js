3091e81c192ffbf76a1e12c0d03a44ead6d65696
Paul Sherman <paul.sherman.88@gmail.com>
2019-02-20 09:47:37 -0600

    Karma tests throw if BrowserStack env vars not set (local)

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,15 @@ const resolve = require("rollup-plugin-node-resolve");
 const customLaunchers = require("./config/karmaLaunchers");
 
 module.exports = function(config) {
+  const username = process.env.BROWSER_STACK_USERNAME;
+  const accessKey = process.env.BROWSER_STACK_ACCESS_KEY;
+
+  if (!username || !accessKey) {
+    throw new Error(
+      "BrowserStack environment variables not set; cannot run tests."
+    );
+  }
+
   config.set({
     frameworks: ["jasmine"],
     reporters: ["progress", "BrowserStack"],
@@ -18,8 +27,8 @@ module.exports = function(config) {
 
     browserStack: {
       project: "hickory",
-      username: process.env.BROWSER_STACK_USERNAME,
-      accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
+      username: username,
+      accessKey: accessKey,
       build: process.env.TRAVIS_BUILD_NUMBER || "local",
       name: process.env.TRAVIS_JOB_NUMBER || "local",
       video: false,
