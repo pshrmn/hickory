6f8aa9bcbd19a40d1d542ebfb5d81ed210d74217
Paul Sherman <paul.sherman.88@gmail.com>
2019-02-20 09:58:21 -0600

    Move all karma config to same file

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,77 @@
 const typescript = require("rollup-plugin-typescript2");
 const resolve = require("rollup-plugin-node-resolve");
-const customLaunchers = require("./config/karmaLaunchers");
+
+const customLaunchers = {
+  // windows
+  bs_chrome: {
+    base: "BrowserStack",
+    browser: "chrome",
+    browser_version: "63",
+    os: "Windows",
+    os_version: "10"
+  },
+  bs_firefox: {
+    base: "BrowserStack",
+    browser: "firefox",
+    browser_version: "57",
+    os: "Windows",
+    os_version: "10"
+  },
+  bs_ie_11: {
+    base: "BrowserStack",
+    browser: "ie",
+    browser_version: "11.0",
+    os: "Windows",
+    os_version: "10"
+  },
+  bs_edge: {
+    base: "BrowserStack",
+    browser: "edge",
+    browser_version: "16",
+    os: "Windows",
+    os_version: "10"
+  },
+  // os x
+  bs_osx_safari: {
+    base: "BrowserStack",
+    browser: "safari",
+    browser_version: "12",
+    os: "OS X",
+    os_version: "Mojave"
+  }
+  /*
+  // ios
+  bs_ios_safari: {
+    base: "BrowserStack",
+    browser: "safari",
+    os: "iOS",
+    os_version: "11.0",
+    device: "iPhone 8",
+    real_mobile: false
+  },
+  // android
+  bs_android_chrome: {
+    base: "BrowserStack",
+    browser: "chrome",
+    os: "android",
+    os_version: "7.0",
+    device: "Samsung Galaxy S8",
+    real_mobile: true
+  }
+  */
+};
+
+const tsConfig = {
+  compilerOptions: {
+    lib: ["es5", "es6", "dom"],
+    target: "es5",
+    module: "es6",
+    allowSyntheticDefaultImports: true,
+    moduleResolution: "Node",
+    types: ["jasmine", "node"]
+  },
+  exclude: ["node_modules"]
+};
 
 module.exports = function(config) {
   const username = process.env.BROWSER_STACK_USERNAME;
@@ -54,7 +125,7 @@ module.exports = function(config) {
     rollupPreprocessor: {
       plugins: [
         typescript({
-          tsconfig: "config/tsconfig.json"
+          tsconfigDefaults: tsConfig
         }),
         resolve()
       ],
